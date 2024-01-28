38774624a5c544d106306cdce113f18f5dea1452
Kris-Chil <Kristinemchilson@gmail.com>
2024-01-28 14:17:50 +0100

    sound fix

--- Assets/scripts/Happiness/HappinessMeter.cs
+++ Assets/scripts/Happiness/HappinessMeter.cs
@@ -141,7 +141,7 @@ public class HappinessMeter : MonoBehaviour
 
     private IEnumerator PanelDestroy()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(1);
         Destroy(gameObject);
     }
 }
